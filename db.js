de8b20278ea739ee53647112f7c4bf4e46d1c9cc
Ayoub El Ghazi <ayoub.el-ghazi-ouserbane@etu.umontpellier.fr>
2026-05-05 11:04:01 +0200

    fix bugs render

--- db.js
+++ db.js
@@ -12,4 +12,8 @@ pool.connect()
   .then(() => console.log("✅ Base de données connectée (Neon PostgreSQL)"))
   .catch(err => console.error("❌ Erreur DB:", err));
 
+pool.on('error', (err, client) => {
+  console.error('❌ Erreur inattendue sur un client PostgreSQL inactif:', err);
+});
+
 module.exports = { pool };
